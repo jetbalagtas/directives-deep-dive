73b2f96ceaa351b4a33152fc7904b01d8773cc06
Jet Balagtas <jetbalagtas@gmail.com>
2019-04-10 21:58:49 -0400

    binding to directive properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,29 +1,38 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'green';
+  @Input() defaultTextColor = 'black';
+  @Input() highlightTextColor = 'white';
   @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.color') color: string;
   constructor(
     private renderer: Renderer2,
     private elRef: ElementRef
   ) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+    this.color = this.defaultTextColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'green');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'green');
-    this.backgroundColor = 'green';
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.backgroundColor = this.highlightColor;
+    this.color = this.highlightTextColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    this.backgroundColor = this.defaultColor;
+    this.color = this.defaultTextColor;
   }
 }
